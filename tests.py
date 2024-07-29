4069775c82e2c5f92e086f5a6569dcee97ef7376
Kylene Hill <hillky@engr.orst.edu>
2024-07-29 16:12:29 -0700

    Wrote test4 to check if password contains lowercase character

--- tests.py
+++ tests.py
@@ -18,6 +18,11 @@ class TestCase(unittest.TestCase):
     	message = "Test did NOT pass: {}".format(test_value)
     	self.assertFalse(check_pwd(test_value), message)
 
+    def test4(self):
+    	test_value = 'TEST4LOWERCASE'
+    	message = "Test did NOT pass: {}".format(test_value)
+    	self.assertFalse(check_pwd(test_value), message)
+
 
 if __name__ == '__main__':
     unittest.main()
