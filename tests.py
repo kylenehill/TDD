bfd23dde77f921e79c41d99e2aa94afc7f9aa2cf
Kylene Hill <hillky@engr.orst.edu>
2024-07-29 15:54:57 -0700

    Wrote test2 to test password length, is correctly rejected as logic is not yet implemented

--- tests.py
+++ tests.py
@@ -8,6 +8,11 @@ class TestCase(unittest.TestCase):
         message = "Test did NOT pass: {}".format(test_value)
         self.assertFalse(check_pwd(test_value), message)
 
+    def test2(self):
+    	test_value = 'testpassword123'
+    	message = "Test did NOT pass: {}".format(test_value)
+    	self.assertFalse(check_pwd(test_value), message)
+
 
 if __name__ == '__main__':
     unittest.main()
