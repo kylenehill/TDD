2b19b22755e5bcd777f76b4d2cbbb1c6f96821cf
Kylene Hill <hillky@engr.orst.edu>
2024-07-29 16:35:50 -0700

    Wrote test8 to check if password contains at least one digit, test fails as logic not yet implemented

--- tests.py
+++ tests.py
@@ -38,6 +38,11 @@ class TestCase(unittest.TestCase):
     	message = "Test did NOT pass: {}".format(test_value)
     	self.assertFalse(check_pwd(test_value), message)
 
+    def test8(self):
+    	test_value = 'checkFORanyDIGIT'
+    	message = "Test did NOT pass: {}".format(test_value)
+    	self.assertFalse(check_pwd(test_value), message)
+
 
 if __name__ == '__main__':
     unittest.main()
