f45ff803de4829da2dc44d0d2ccc9d14b6c73c85
Kylene Hill <hillky@engr.orst.edu>
2024-07-29 16:23:18 -0700

    Made check_pwd return False when lowercase character is not present

--- check_pwd.py
+++ check_pwd.py
@@ -5,4 +5,12 @@ def check_pwd(test_value):
 	if len(test_value) < 8 or len(test_value) > 20:
 		return False
 
+	lower_count = 0
+	for char in test_value:
+		if char.islower() == True:
+			lower_count += 1
+
+	if lower_count == 0:
+		return False
+
 	return True
